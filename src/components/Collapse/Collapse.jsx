6f374b6546d64c8033c86733823027531ff2e66a
Aymeric BOISNE-NOC <aymeric.boisnenoc@gmail.com>
2024-06-24 10:23:21 +0200

    ajout animation sur collapse lors du clic pour le déroulement , ajout du scss pour la version mobile

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,27 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import vectorUp from '../../assets/img/vector-up.svg';
-
 
 const Collapse = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
 
   const toggle = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    if (isOpen) {
+      contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
+    } else {
+      contentRef.current.style.maxHeight = '0';
+    }
+  }, [isOpen]);
+
   return (
     <div className='collapse'>
       <button onClick={toggle} className='collapse-button'>
-        {title} 
+        {title}
         <img src={vectorUp} alt='' className={isOpen ? 'rotate' : ''} />
       </button>
-      {isOpen && (
-        <div>
-          {children}
-        </div>
-      )}
+      <div
+        ref={contentRef}
+        className={`collapse-content ${isOpen ? 'open' : ''}`}
+      >
+        {children}
+      </div>
     </div>
   );
 };
 
 export default Collapse;
+
